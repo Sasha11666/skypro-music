42847e74c675cbc9a52b9d97b3e1722e63d5d152
Sasha11666 <basovasasa087@gmail.com>
2023-07-10 18:21:28 +0300

    started create-react-app

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <span className="test-text">Здесь скоро будет Skypro.Music</span>
+    </div>
+  );
+}
+
+export default App;
